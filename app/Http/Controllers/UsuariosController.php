1f39d57ac81a56edd5a686a2ac246efc09e23532
Ronaldo Pires Dias <ronaldo.pires@armac.com.br>
2022-11-05 14:31:38 -0300

    Usuarios

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UsuarioModel;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class UsuariosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return User::all();
+        // return "CONECTADO";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        DB::transaction(function () use ($request) {
+            $user = User::create([
+                'name' => $request["name"],
+                'email' => $request["email"],
+                'password' => $request["password"]
+            ]);
+        });
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Usuario $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, User $usuario)
+    {
+        return $usuario;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Usuario $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $usuario)
+    {
+        DB::transaction(function () use ($request, $usuario) {
+            $usuario->update($request->all());
+        });
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Usuario $usuario
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $usuario)
+    {
+        $usuario->ativo = false;
+        $usuario->save();
+    }
+}
